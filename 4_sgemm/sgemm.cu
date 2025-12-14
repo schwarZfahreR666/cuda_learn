950ffe5de795e2caf7906037b5502ca54d4dccee
schwarZfahreR666 <qqq190979389@163.com>
2025-12-14 18:29:02 +0800

    pingpong buffer sgemm

--- 4_sgemm/sgemm.cu
+++ 4_sgemm/sgemm.cu
@@ -373,13 +373,203 @@ __global__ void outer_sgemm_register_float4(float* A, float* B, float* C, const 
         FETCH_FLOAT4(c_ptr_start[(ty * M_NUM_PER_THREAD + i) * N + tx * N_NUM_PER_THREAD]) = FETCH_FLOAT4(temp[i][0]);
     }
 }
+//对A做转置后存储到smem
+template<unsigned int M_NUM_PER_BLOCK, 
+        unsigned int N_NUM_PER_BLOCK, 
+        unsigned int K_NUM_PER_BLOCK, 
+        unsigned int M_NUM_PER_THREAD,
+        unsigned int N_NUM_PER_THREAD>
+__global__ void outer_sgemm_register_float4_transpose(float* A, float* B, float* C, const int M, const int N, const int K){
+    //每个block负责数据的单个维度
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+    
+    float* A_ptr_start = A + M_NUM_PER_BLOCK * blockIdx.y * K;
+    float* B_ptr_start = B + N_NUM_PER_BLOCK * blockIdx.x;
+    //用于load数据时暂存做转置,大小为K_NUM_PER_THREAD
+    float a_reg_load[4] = {0.f};
+
+    __shared__ float a_shared[K_NUM_PER_BLOCK][M_NUM_PER_BLOCK];
+    __shared__ float b_shared[K_NUM_PER_BLOCK][N_NUM_PER_BLOCK];
+    
+    //每个线程依然负责计算NUM_PER_THREAD个数
+    float temp[M_NUM_PER_THREAD][N_NUM_PER_THREAD] = {0.f};
+    float a_reg[M_NUM_PER_THREAD] = {0.f};
+    float b_reg[N_NUM_PER_THREAD] = {0.f};
+    
+    int K_NUM_PER_THREAD = 4; //这个大小是由float4指令决定的
+    for(int s = 0; s < K; s += K_NUM_PER_BLOCK){
+        //每次取4个数，要取4次，因为A的取次数要与B的一次取得数相匹配，都为4
+        for(int i = 0; i < M_NUM_PER_THREAD; i++){
+            //先把数据加载到寄存器中
+            FETCH_FLOAT4(a_reg_load[0]) = FETCH_FLOAT4(A_ptr_start[(ty * M_NUM_PER_THREAD + i) * K + s + tx * K_NUM_PER_THREAD]); //在y方向，每个线程负责加载M_NUM_PER_THREAD行数据，所以要乘这个值
+            //在global内存中，tx对应K维，ty对应M维;在转置到smem后，两维索引需要交换位置，但计算方式不变
+            a_shared[tx * K_NUM_PER_THREAD + 0][ty * M_NUM_PER_THREAD + i] = a_reg_load[0];
+            a_shared[tx * K_NUM_PER_THREAD + 1][ty * M_NUM_PER_THREAD + i] = a_reg_load[1];
+            a_shared[tx * K_NUM_PER_THREAD + 2][ty * M_NUM_PER_THREAD + i] = a_reg_load[2];
+            a_shared[tx * K_NUM_PER_THREAD + 3][ty * M_NUM_PER_THREAD + i] = a_reg_load[3];
+        }
+        for(int i = 0; i < K_NUM_PER_THREAD; i++){
+            FETCH_FLOAT4(b_shared[ty * K_NUM_PER_THREAD + i][tx * N_NUM_PER_THREAD]) = FETCH_FLOAT4(B_ptr_start[(ty * K_NUM_PER_THREAD + s + i) * N + tx * N_NUM_PER_THREAD]);
+        }
+        
+            
+        __syncthreads();
+        //计算小矩阵之间的mm
+        for(int k = 0; k < K_NUM_PER_BLOCK; k++){
+            //先把固定k的位置的数加载到寄存器
+            //一共64*64个数，16*16个线程。
+            //每个线程负责计算16个数，x、y方向各加载4个数
+            //计算时需要看目标矩阵的tile
+            FETCH_FLOAT4(a_reg[0]) = FETCH_FLOAT4(a_shared[k][ty * M_NUM_PER_THREAD]);
+            //B取行，可以使用float4取数
+            FETCH_FLOAT4(b_reg[0]) = FETCH_FLOAT4(b_shared[k][tx * N_NUM_PER_THREAD]);
+            
+            for(int i = 0; i < M_NUM_PER_THREAD; i++){
+                for(int j = 0; j < N_NUM_PER_THREAD; j++){
+                    temp[i][j] += a_reg[i] * b_reg[j];
+                }
+            }
+        }
+        
+        __syncthreads();
+    }
+    float* c_ptr_start = C + blockIdx.y * N * M_NUM_PER_BLOCK + blockIdx.x * N_NUM_PER_BLOCK;
+    // for(int i = 0; i < M_NUM_PER_THREAD; i++){
+    //     for(int j = 0; j < N_NUM_PER_THREAD; j++){
+    //         //每个维度考虑全局block偏移+block内偏移
+    //         //每个thread将负责计算的16个数写回全局内存
+    //         //block偏移已经算好了，计算block内偏移即可
+    //         c_ptr_start[(ty * M_NUM_PER_THREAD + i) * N + tx * N_NUM_PER_THREAD + j] = temp[i][j];
+    //     }    
+    // }
+    //写入也可以使用float4
+    for(int i = 0; i < M_NUM_PER_THREAD; i++){
+        FETCH_FLOAT4(c_ptr_start[(ty * M_NUM_PER_THREAD + i) * N + tx * N_NUM_PER_THREAD]) = FETCH_FLOAT4(temp[i][0]);
+    }
+}
+
+//对A做转置后存储到smem
+template<unsigned int M_NUM_PER_BLOCK, 
+        unsigned int N_NUM_PER_BLOCK, 
+        unsigned int K_NUM_PER_BLOCK, 
+        unsigned int M_NUM_PER_THREAD,
+        unsigned int N_NUM_PER_THREAD>
+__global__ void sgemm_pingpong(float* A, float* B, float* C, const int M, const int N, const int K){
+    //每个block负责数据的单个维度
+    int bx = blockIdx.x;
+    int by = blockIdx.y;
+
+    int tx = threadIdx.x;
+    int ty = threadIdx.y;
+
+    const int tid = ty * blockDim.x + tx;  //拿到block内全局索引
+
+    __shared__ float a_shared[2][K_NUM_PER_BLOCK][M_NUM_PER_BLOCK];
+    __shared__ float b_shared[2][K_NUM_PER_BLOCK][N_NUM_PER_BLOCK];
+
+    //每个线程依然负责计算NUM_PER_THREAD个数
+    float temp[M_NUM_PER_THREAD][N_NUM_PER_THREAD] = {0.f};
+    float a_reg[M_NUM_PER_THREAD] = {0.f};
+    float b_reg[N_NUM_PER_THREAD] = {0.f};
+    //用于load数据时暂存做转置,大小为K_NUM_PER_THREAD
+    float a_reg_load[4] = {0.f};
+    
+    float* A_ptr_start = A + M_NUM_PER_BLOCK * blockIdx.y * K;
+    float* B_ptr_start = B + N_NUM_PER_BLOCK * blockIdx.x;
+
+    const int A_tile_thread_per_row = K_NUM_PER_BLOCK / 4;// 8 // 4 = 2,4是float4的字节数
+    const int B_tile_thread_per_row = N_NUM_PER_BLOCK / 4;// 128 // 4 = 32,4是float4的字节数
+    //对block全局线程id做一个重排，A和B的buffer形状不同
+    const int A_tile_tid_x = tid % A_tile_thread_per_row;  //取值0-1
+    const int A_tile_tid_y = tid / A_tile_thread_per_row;  //取值 0-127
+
+    const int B_tile_tid_x = tid % B_tile_thread_per_row;  //取值 0-31
+    const int B_tile_tid_y = tid / B_tile_thread_per_row;  //取值 0-7
+
+    int K_NUM_PER_THREAD = 4; //这个大小是由float4指令决定的
+
+    //取第一块内存
+    FETCH_FLOAT4(a_reg_load[0]) = FETCH_FLOAT4(A_ptr_start[A_tile_tid_y * K + A_tile_tid_x * K_NUM_PER_THREAD]);
+    //做了转置，所以列id都是一样的
+    a_shared[0][A_tile_tid_x * K_NUM_PER_THREAD + 0][A_tile_tid_y] = a_reg_load[0];
+    a_shared[0][A_tile_tid_x * K_NUM_PER_THREAD + 1][A_tile_tid_y] = a_reg_load[1];
+    a_shared[0][A_tile_tid_x * K_NUM_PER_THREAD + 2][A_tile_tid_y] = a_reg_load[2];
+    a_shared[0][A_tile_tid_x * K_NUM_PER_THREAD + 3][A_tile_tid_y] = a_reg_load[3];
+
+    FETCH_FLOAT4(b_shared[0][B_tile_tid_y][B_tile_tid_x * K_NUM_PER_THREAD]) = FETCH_FLOAT4(B_ptr_start[B_tile_tid_y * N + B_tile_tid_x * K_NUM_PER_THREAD]);
+    __syncthreads();
+    int write_buffer_idx = 1;
+    for(int s = K_NUM_PER_BLOCK; s < K; s += K_NUM_PER_BLOCK){
+        //继续取第二块buffer的内存,和之前一样，只是需要在K方向多偏移一个s
+        FETCH_FLOAT4(a_reg_load[0]) = FETCH_FLOAT4(A_ptr_start[A_tile_tid_y * K + A_tile_tid_x * K_NUM_PER_THREAD + s]);
+        //做了转置，所以列id都是一样的
+        a_shared[write_buffer_idx][A_tile_tid_x * K_NUM_PER_THREAD + 0][A_tile_tid_y] = a_reg_load[0];
+        a_shared[write_buffer_idx][A_tile_tid_x * K_NUM_PER_THREAD + 1][A_tile_tid_y] = a_reg_load[1];
+        a_shared[write_buffer_idx][A_tile_tid_x * K_NUM_PER_THREAD + 2][A_tile_tid_y] = a_reg_load[2];
+        a_shared[write_buffer_idx][A_tile_tid_x * K_NUM_PER_THREAD + 3][A_tile_tid_y] = a_reg_load[3];
+
+        FETCH_FLOAT4(b_shared[write_buffer_idx][B_tile_tid_y][B_tile_tid_x * K_NUM_PER_THREAD]) = FETCH_FLOAT4(B_ptr_start[(B_tile_tid_y + s) * N + B_tile_tid_x * K_NUM_PER_THREAD]);
+        //改变当前流水级别buffer idx
+        write_buffer_idx ^= 1;
+        //计算小矩阵之间的mm
+        for(int k = 0; k < K_NUM_PER_BLOCK; k++){
+            //计算时需要看目标矩阵的tile
+            FETCH_FLOAT4(a_reg[0]) = FETCH_FLOAT4(a_shared[write_buffer_idx][k][ty * M_NUM_PER_THREAD]);
+            FETCH_FLOAT4(a_reg[4]) = FETCH_FLOAT4(a_shared[write_buffer_idx][k][ty * M_NUM_PER_THREAD + 4]);
+            //B取行，可以使用float4取数
+            FETCH_FLOAT4(b_reg[0]) = FETCH_FLOAT4(b_shared[write_buffer_idx][k][tx * N_NUM_PER_THREAD]);
+            FETCH_FLOAT4(b_reg[4]) = FETCH_FLOAT4(b_shared[write_buffer_idx][k][tx * N_NUM_PER_THREAD + 4]);
+            
+            for(int i = 0; i < M_NUM_PER_THREAD; i++){
+                for(int j = 0; j < N_NUM_PER_THREAD; j++){
+                    temp[i][j] += a_reg[i] * b_reg[j];
+                }
+            }
+        }
+        
+        __syncthreads();
+    }
+    write_buffer_idx ^= 1;
+    //计算最后一个小矩阵之间的mm
+    for(int k = 0; k < K_NUM_PER_BLOCK; k++){
+        //计算时需要看目标矩阵的tile
+        FETCH_FLOAT4(a_reg[0]) = FETCH_FLOAT4(a_shared[write_buffer_idx][k][ty * M_NUM_PER_THREAD]);
+        FETCH_FLOAT4(a_reg[4]) = FETCH_FLOAT4(a_shared[write_buffer_idx][k][ty * M_NUM_PER_THREAD + 4]);
+        //B取行，可以使用float4取数
+        FETCH_FLOAT4(b_reg[0]) = FETCH_FLOAT4(b_shared[write_buffer_idx][k][tx * N_NUM_PER_THREAD]);
+        FETCH_FLOAT4(b_reg[4]) = FETCH_FLOAT4(b_shared[write_buffer_idx][k][tx * N_NUM_PER_THREAD + 4]);
+        
+        for(int i = 0; i < M_NUM_PER_THREAD; i++){
+            for(int j = 0; j < N_NUM_PER_THREAD; j++){
+                temp[i][j] += a_reg[i] * b_reg[j];
+            }
+        }
+    }
+    __syncthreads();
+
+    float* c_ptr_start = C + blockIdx.y * N * M_NUM_PER_BLOCK + blockIdx.x * N_NUM_PER_BLOCK;
+    // for(int i = 0; i < M_NUM_PER_THREAD; i++){
+    //     for(int j = 0; j < N_NUM_PER_THREAD; j++){
+    //         //每个维度考虑全局block偏移+block内偏移
+    //         //每个thread将负责计算的16个数写回全局内存
+    //         //block偏移已经算好了，计算block内偏移即可
+    //         c_ptr_start[(ty * M_NUM_PER_THREAD + i) * N + tx * N_NUM_PER_THREAD + j] = temp[i][j];
+    //     }    
+    // }
+    //写入也可以使用float4
+    for(int i = 0; i < M_NUM_PER_THREAD; i++){
+        FETCH_FLOAT4(c_ptr_start[(ty * M_NUM_PER_THREAD + i) * N + tx * N_NUM_PER_THREAD]) = FETCH_FLOAT4(temp[i][0]);
+        FETCH_FLOAT4(c_ptr_start[(ty * M_NUM_PER_THREAD + i) * N + tx * N_NUM_PER_THREAD + 4]) = FETCH_FLOAT4(temp[i][4]);
+    }
+}
 
 int main(){
     double iStart, iElaps;
 
-    int m = 1024;
-    int n = 1024;
-    constexpr int k = 1024;
+    int m = 2048;
+    int n = 2048;
+    constexpr int k = 2048;
     const size_t mem_size_A = m * k * sizeof(float);
     const size_t mem_size_B = k * n * sizeof(float);
     const size_t mem_size_C = m * n * sizeof(float);
@@ -531,6 +721,45 @@ int main(){
     cudaDeviceSynchronize();
     max_diff = compare_matrices(m, n, matrix_C_host_cpu, matrix_C_host_gpu);
     printf("outer_sgemm_register_float4 time: %lf , max_diff: %f\n", iElaps, max_diff);
+
+    cudaMemset(matrix_C_device, 0, mem_size_C);
+    memset(matrix_C_host_gpu, 0, mem_size_C);
+    iStart = cpuSecond();
+    outer_sgemm_register_float4_transpose<M_NUM_PER_BLOCK_OUTER, N_NUM_PER_BLOCK_OUTER, K_NUM_PER_BLOCK_OUTER, M_NUM_PER_THREAD_OUTER, N_NUM_PER_THREAD_OUTER><<<grid_float4_outer, block_float4_outer>>>(matrix_A_device, matrix_B_device, matrix_C_device, m, n, k);
+    cudaDeviceSynchronize();
+    iElaps = cpuSecond() - iStart;
+    cudaMemcpy(matrix_C_host_gpu, matrix_C_device, mem_size_C, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    max_diff = compare_matrices(m, n, matrix_C_host_cpu, matrix_C_host_gpu);
+    printf("outer_sgemm_register_float4_transpose time: %lf , max_diff: %f\n", iElaps, max_diff);
+
+
+    //先规定好每个block在每个维度处理多少个数(从目标矩阵看)
+    constexpr int M_NUM_PER_BLOCK_PINGPONG = 128;
+    constexpr int N_NUM_PER_BLOCK_PINGPONG = 128;
+    constexpr int K_NUM_PER_BLOCK_PINGPONG = 8;
+    
+    constexpr int NUM_PER_THREAD_PINGPONG = 64; //每个thread输出几个结果
+    constexpr int M_NUM_PER_THREAD_PINGPONG = 8;  //每个线程在每个维度计算几个数，两个乘积应该是NUM_PER_THREAD_OUTER
+    constexpr int N_NUM_PER_THREAD_PINGPONG = 8;
+    constexpr size_t ROW_SIZE_PINGPONG = M_NUM_PER_BLOCK_PINGPONG / M_NUM_PER_THREAD_PINGPONG;   
+    constexpr size_t COL_SIZE_PINGPONG = N_NUM_PER_BLOCK_PINGPONG / N_NUM_PER_THREAD_PINGPONG;
+    dim3 block_pingpong(ROW_SIZE_PINGPONG, COL_SIZE_PINGPONG);
+    
+    //二维网格，分别也和m、n的大小有关
+    //只使用原来的四分之一block
+    //顺序为先x后y
+    dim3 grid_pingpong((n + N_NUM_PER_BLOCK_PINGPONG - 1) / N_NUM_PER_BLOCK_PINGPONG, (m + M_NUM_PER_BLOCK_PINGPONG - 1) / M_NUM_PER_BLOCK_PINGPONG);
+    cudaMemset(matrix_C_device, 0, mem_size_C);
+    memset(matrix_C_host_gpu, 0, mem_size_C);
+    iStart = cpuSecond();
+    sgemm_pingpong<M_NUM_PER_BLOCK_PINGPONG, N_NUM_PER_BLOCK_PINGPONG, K_NUM_PER_BLOCK_PINGPONG, M_NUM_PER_THREAD_PINGPONG, N_NUM_PER_THREAD_PINGPONG><<<grid_pingpong, block_pingpong>>>(matrix_A_device, matrix_B_device, matrix_C_device, m, n, k);
+    cudaDeviceSynchronize();
+    iElaps = cpuSecond() - iStart;
+    cudaMemcpy(matrix_C_host_gpu, matrix_C_device, mem_size_C, cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    max_diff = compare_matrices(m, n, matrix_C_host_cpu, matrix_C_host_gpu);
+    printf("sgemm_pingpong time: %lf , max_diff: %f\n", iElaps, max_diff);
     
     
     free(matrix_A_host);
